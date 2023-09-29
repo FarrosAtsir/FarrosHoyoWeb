7e0289964650050700461adc43d656e4d96d1c24
FarrosAtsir <ayanokouji120108@gmail.com>
2023-09-29 14:05:44 +0700

    kedua

--- javascript.js
+++ javascript.js
@@ -18,3 +18,11 @@ ScrollReveal().reveal('.home-text, .about-img, .footer-text', { origin: 'left' }
 ScrollReveal().reveal('.home-img, .about-text, .footer-icon-top', { origin: 'right' });
 ScrollReveal().reveal('.heading', { origin: 'top' });
 ScrollReveal().reveal('.box, .col', { origin: 'bottom' });
+
+const typed = new Typed('.multiple-text',{
+    strings:['Honkai Impact 3rd', 'Genshin Impact', 'Tears Of Themis', 'Honkai: Star Rail'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
